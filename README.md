18de066af073f478e5d5e169fa383928db50236a
SilentFrogNet <info@silentfrog.net>
2018-09-04 11:38:04 +0200

    Added README and more spinners

---
+++ README.md
@@ -0,0 +1,14 @@
+# Spinner
+
+This is a really tiny library to add spinners to your code.
+
+## Spinner types
+
+  * CLASSIC
+  * SINGLE_PIPES
+  * DOUBLE_PIPES
+  * VERTICAL
+  * DOT
+  * DOTS
+  * DOTS2
+  
--- spin_test.py
+++ spin_test.py
@@ -1,15 +1,16 @@
 import threading
 import time
+import sys
 
-from metagoofil2.utils.utils import Spinner
+from spinner.spinner import Spinner
 
 
-def print_spinner(text, spin_type):
+def print_spinner(text, spin_type, sleep=4):
     stop_spinner = threading.Event()
     spin_thread = Spinner(stop_spinner, prefix=text, spinner_string=spin_type)
     spin_thread.start()
 
-    time.sleep(4)
+    time.sleep(sleep)
 
     stop_spinner.set()
     spin_thread.join()
@@ -18,9 +19,19 @@ def print_spinner(text, spin_type):
     print(f"{text}DONE\n")
 
 
-print_spinner('classic...', Spinner.SpinnerTypes.CLASSIC)
-print_spinner('single...', Spinner.SpinnerTypes.SINGLE_PIPES)
-print_spinner('double...', Spinner.SpinnerTypes.DOUBLE_PIPES)
-print_spinner('vertical...', Spinner.SpinnerTypes.VERTICAL)
+def main(sleep):
+    print_spinner('classic...', Spinner.SpinnerTypes.CLASSIC, sleep=sleep)
+    print_spinner('single...', Spinner.SpinnerTypes.SINGLE_PIPES, sleep=sleep)
+    print_spinner('double...', Spinner.SpinnerTypes.DOUBLE_PIPES, sleep=sleep)
+    print_spinner('vertical...', Spinner.SpinnerTypes.VERTICAL, sleep=sleep)
+    print_spinner('dot', Spinner.SpinnerTypes.DOT, sleep=sleep)
+    print_spinner('dots', Spinner.SpinnerTypes.DOTS, sleep=sleep)
+    print_spinner('dots2', Spinner.SpinnerTypes.DOTS2, sleep=sleep)
 
 
+if __name__ == '__main__':
+    sleep = 4
+    if len(sys.argv) > 1:
+        sleep = int(sys.argv[1])
+
+    main(sleep)
